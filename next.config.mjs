010dd12dd9f865f7d7149781a8eef98989d71238
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2026-02-16 16:41:27 +0530

    16_02_next_cofig.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    unoptimized: true,   
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.inforbit.in",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
