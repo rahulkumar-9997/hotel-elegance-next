15f10e54138015bd48c0f99c21be57a64c3bd730
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2026-02-16 13:30:08 +0530

    16_02_2026_ci_cd_pipelines

--- next.config.js
+++ next.config.js
@@ -12,30 +12,30 @@ const nextConfig = {
     ],
   },
 
-  async redirects() {
-    return [
-      {
-        source: "/restaurant",
-        destination: "/budget-restaurant-varanasi",
-        permanent: true,
-      },
-      {
-        source: "/banquet",
-        destination: "/best-banquet-budget-varanasi",
-        permanent: true,
-      },
-      {
-        source: "/tarriff",
-        destination: "/tariff",
-        permanent: true,
-      },
-      {
-        source: "/:slug*.php",
-        destination: "/:slug*",
-        permanent: true,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/restaurant",
+  //       destination: "/budget-restaurant-varanasi",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/banquet",
+  //       destination: "/best-banquet-budget-varanasi",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/tarriff",
+  //       destination: "/tariff",
+  //       permanent: true,
+  //     },
+  //     {
+  //       source: "/:slug*.php",
+  //       destination: "/:slug*",
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
