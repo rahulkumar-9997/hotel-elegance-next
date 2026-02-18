78363ab0f002baecaac972d73fa82cb3bfa062c3
rahulkumar-9997 <rahulkumarmaurya464@gmail.com>
2026-02-18 13:47:18 +0530

    18_02_2026_server.js_file_update

--- server.js
+++ server.js
@@ -1,12 +1,1 @@
-const http = require('http')
-const next = require('next')
-
-const port = process.env.PORT || 3000
-const app = next({ dev: false })
-const handle = app.getRequestHandler()
-
-app.prepare().then(() => {
-  http.createServer((req, res) => {
-    handle(req, res)
-  }).listen(port)
-})
+require("./.next/standalone/server.js");
